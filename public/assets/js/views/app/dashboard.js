d1cc16120f464870ed19066dac3f61f57f00e8e3
Gingertek <38133930+ginger-tek@users.noreply.github.com>
2025-07-12 03:00:55 +0000

    added luxon for dates

--- public/assets/js/views/app/dashboard.js
+++ public/assets/js/views/app/dashboard.js
@@ -1,4 +1,5 @@
 import Chart from '../../components/chart.js'
+import { Info } from '/nm/luxon/build/es6/luxon.js'
 
 export default {
   components: { Chart },
@@ -8,15 +9,16 @@ export default {
   </div>`,
   setup() {
     const enrollmentChartData = Vue.ref({
-      labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+      labels: Info.monthsFormat('short'),
       datasets: [{
-        label: 'Enrollments',
-        data: [30, 20, 50, 40, 60, 70],
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        borderColor: 'rgba(75, 192, 192, 1)',
-        borderWidth: 1
+        label: 'Students',
+        data: [10, 25, 15, 20, 50, 40, 55, 57, 35, 40, 37, 35],
+        backgroundColor: 'rgba(27, 142, 250, 1)',
+        borderColor: 'rgba(27, 142, 250, 1)',
+        borderWidth: 2
       }]
     })
+
     return {
       enrollmentChartData
     }
