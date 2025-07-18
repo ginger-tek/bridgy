89840f1879ab882b423bd6e299b96aa2509e8ee4
ginger-tek <moormann.jeremy.tech@gmail.com>
2025-07-17 21:42:54 -0400

    change icon

--- public/assets/js/views/app.js
+++ public/assets/js/views/app.js
@@ -5,7 +5,7 @@ export default {
         <nav>
           <ul>
             <li><router-link to="/app"><i class="bi bi-graph-up"></i> Dashboard</router-link></li>
-            <li><router-link to="/app/classes"><i class="bi bi-list-task"></i> Classes</router-link></li>
+            <li><router-link to="/app/classes"><i class="bi bi-journals"></i> Classes</router-link></li>
             <li><router-link to="/app/students"><i class="bi bi-person-lines-fill"></i> Students</router-link></li>
             <li><router-link to="/app/people"><i class="bi bi-people-fill"></i> People</router-link></li>
             <li><router-link to="/app/settings"><i class="bi bi-gear-fill"></i> Settings</router-link></li>
